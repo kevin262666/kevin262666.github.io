7ab69278413746909584ce286df71a5b01983ba7
魏孝峯 <kevin262666@gmail.com>
2020-04-13 12:05:03 +0800

    新增demo用的js檔

---
+++ demoUs/demoUs.js
@@ -0,0 +1,9 @@
+// demo網站的alert
+var alertDemoText = document.createElement('div'),
+    alertDemoText_text = document.createTextNode('DEMO用,皆非正式資料'),
+    alertDemoText_css = 'font-size: 12px; position: fixed; bottom: 0px; left: 0px; width: 100%; background: #f00; color: #fff; text-align: center;';
+
+alertDemoText.setAttribute('class', 'alertDemoText');
+alertDemoText.setAttribute('style', alertDemoText_css);
+alertDemoText.appendChild(alertDemoText_text);
+document.querySelector('body').appendChild(alertDemoText);
